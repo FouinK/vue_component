1f58addd8237bf43450242947431d7cdd168f265
Fouink <yyss9103@gmail.com>
2023-05-18 13:56:25 +0900

    first commit

---
+++ src/views/JoinPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+      <div>
+          <input placeholder="아이디"><br>
+          <input placeholder="비밀번호" type="password"><br>
+          <button>Join</button>
+      </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
