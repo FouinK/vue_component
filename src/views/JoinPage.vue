9fccbc64ab7936828dabccb5a6bd62b96a5a742a
Fouink <yyss9103@gmail.com>
2023-05-19 10:10:01 +0900

    API 통신 테스트

--- src/views/JoinPage.vue
+++ src/views/JoinPage.vue
@@ -1,13 +1,44 @@
-<script setup>
+<script>
 
+  import axios from "axios";
+
+  export default {
+      name: 'restAPI',
+      methods: {
+          getData() {
+              const config = {
+                  headers: {
+                  },params: {
+                      order: 'ADMIN_SETTING',
+                  },
+              };
+
+              this.name = '회원가입 완료 !';
+              alert("회원가입이 완료 되었습니다");
+
+              axios.get('https://shop-api.e-ncp.com/display/banners/001', config)
+                  .then((result) => {
+                      console.log(result);
+                      this.data = result;
+                  });
+          },
+      },
+      data() {
+          return {
+              data: {},
+
+          }
+      }
+  }
 </script>
 
 <template>
   <main>
+      {{name}}
       <div>
           <input placeholder="아이디"><br>
           <input placeholder="비밀번호" type="password"><br>
-          <button>Join</button>
+          <button @click="getData">Join</button>
       </div>
   </main>
 </template>
