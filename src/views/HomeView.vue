b79eac036185a8e5f889cda23a9b159ee3cd0a5d
Fouink <yyss9103@gmail.com>
2023-05-19 17:52:30 +0900

    처음부터 다시 연습

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,65 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+
+import axios from "axios";
+
+export default {
+    name: 'restAPI',
+    data() {
+        return {
+            accounts: [],
+            eventResult: {},
+        }
+    },
+    methods: {
+        async getMain() {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json"
+                }, params: {},
+            };
+
+            const result = await axios.get('http://localhost:21010/client/v1/display/get-main-menu?cache=true', config)
+            console.log(result.data.mainMenu.accounts);
+            this.accounts = result.data.mainMenu.accounts;
+        },
+        async getEvent(eventNo) {
+            const config = {
+                headers: {
+                    "Content-Type": "application/json"
+                }, params: {
+                    order: 'ADMIN_SETTING',
+                    soldout: true
+                },
+            };
+
+            const result = await axios.get(`http://localhost:21010/client/v1/display/get-main-menu/product/${eventNo}`, config)
+            console.log(result.data);
+            this.eventResult = result.data;
+        },
+
+    },
+    created() {
+        this.getMain().then(() => {
+            this.accounts.forEach(account => account.banners.forEach(
+                banner => {
+                    this.getEvent(banner.landingUrl);
+                }
+            ))
+        });
+
+
+    }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <div v-for="account in accounts" :key="account.name">
+            {{ account.accountName }}
+        </div>
+
+        <div v-for="event in eventResult" :key="event.section">
+            {{event.section}}
+        </div>
+    </main>
 </template>
--- src/views/JoinPage.vue
+++ src/views/JoinPage.vue
@@ -1,35 +1,5 @@
 <script>
 
-  import axios from "axios";
-
-  export default {
-      name: 'restAPI',
-      methods: {
-          getData() {
-              const config = {
-                  headers: {
-                  },params: {
-                      order: 'ADMIN_SETTING',
-                  },
-              };
-
-              this.name = '회원가입 완료 !';
-              alert("회원가입이 완료 되었습니다");
-
-              axios.get('https://shop-api.e-ncp.com/display/banners/001', config)
-                  .then((result) => {
-                      console.log(result);
-                      this.data = result;
-                  });
-          },
-      },
-      data() {
-          return {
-              data: {},
-
-          }
-      }
-  }
 </script>
 
 <template>
